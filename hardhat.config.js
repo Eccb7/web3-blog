6ad64b55533ff7ea2c263b1ea8f7b2199d609755
Briton Otieno <ojwangbriton@gmail.com>
2024-01-25 13:55:53 +0300

    Configure local hardhat dev environment

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,17 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    // mumbai: {
+    //   url: "https://rpc-mumbai.matic.today",
+    //   accounts: [process.env.pk]
+    // },
+    // polygon: {
+    //   url: "https://polygon-rpc.com/",
+    //   accounts: [process.env.pk]
+    // }
+  }
 };
